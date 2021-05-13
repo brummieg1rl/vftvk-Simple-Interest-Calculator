0ee992e42ca811104526606f62cf99e117c805f3
brummieg1rl <82397061+brummieg1rl@users.noreply.github.com>
2021-05-13 11:15:09 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute(){
+	var principal=document.getElementById("principal").value;
+	var rate=document.getElementById("rate").value;
+	var years=document.getElementById("years").value;
+	var year=new Date().getFullYear()+parseInt(years);
 }
-        
